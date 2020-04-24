0be0998683a08f48a53ea24a0193409b53f6d0dd
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-04-24 01:22:42 -0400

    Updated
    Questions:
    1) is the `geodataframe_points` argument already a gdf?  Why do we have to read it in instead of doing something like
    `points_gdf = geodataframe_points'?
    
    2)  Could you maybe explain the big differences between op='intersects' and op='within'?

--- tools/state_ids.py
+++ tools/state_ids.py
@@ -5,10 +5,26 @@ def get_state_ids(
         geodataframe_points,
         states_geojson='data/input/states.json',
 ):
+    # states_gepjson is from:
     # https://raw.githubusercontent.com/loganpowell/census-geojson/master/GeoJSON/5m/2018/state.json
 
+
     # read geodataframe_points
+    points_gdf = gpd.read_file(geodataframe_points)
+
+    # read states_geojson (crs -> epsg:4326)
+    states_gdf = gpd.read_file(states_geojson)[['STATEFP', 'STUSPS', 'NAME', 'geometry']]
+
+    # matching both gdf's crs just in case
+    points_gdf.to_crs(states_gdf.crs, inplace=True)
 
     # spatial join to get associate state ids with address points
+    points_sj_states = gpd.sjoin(
+        points_gdf,
+        states_gdf,
+        how='left',
+        op='intersects',    # within <-opposite-> contains
+                            # points are within the states
+    )
 
     return points_sj_states
