f82de483e5a107ab25b3e03fbe0c8284cd318d5f
Anna Guidi <anna.guidi@nytimes.com>
2020-04-25 23:39:26 +0200

    completed code

--- tools/geocode.py
+++ tools/geocode.py
@@ -1,5 +1,5 @@
 from geopy.geocoders import Nominatim
-from shapely.geometry import Point  # importing the Point class
+from shapely.geometry import Point
 from geopandas import GeoDataFrame
 
 
@@ -9,14 +9,21 @@ geolocator = Nominatim(user_agent='pratt_geospatial')
 def geocode_dataframe(address_dataframe, address_column):
     df = address_dataframe.copy()
 
-    # geocode address column by apply-ing geolocator.geocode 
+
+    # geocode address column by apply-ing geolocator.geocode
+    df['geocode'] = df[address_column].apply(geolocator.geocode)
 
     # create a geometry column
+    df['geometry'] = df['geocode'].apply(
+        lambda x:
+        Point(x.longitude, x.latitude)
+    )
 
     # create a geodataframe called gdf from df
+    # define the .crs =  {'init': 'epsg:4326'}
+    gdf = GeoDataFrame(df, geometry='geometry', crs='epsg:4326')
 
-    # define the .crs =  {'init': 'epsg:4326'} 
-
-    # drop the geocode column from the gdf  
+    # drop the geocode column from the gdf
+    gdf.drop(['geocode'], axis=1)
 
     return gdf
