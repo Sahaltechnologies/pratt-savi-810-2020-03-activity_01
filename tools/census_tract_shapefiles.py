9c69617f72e4c300c2fa9b5ee3817bbd7835a94d
Danny S <daniel.martin.sheehan@gmail.com>
2020-05-04 17:25:58 -0400

    update

--- tools/census_tract_shapefiles.py
+++ tools/census_tract_shapefiles.py
@@ -10,6 +10,10 @@ def get_census_tracts(geoid, download_directory):
     download_url = f'{url_dir}/tl_2019_{geoid}_tract.zip'
     saved_file = f'{download_directory}/tl_2019_{geoid}_tract.zip'
 
-    # download the file from the download url
+    if not os.path.exists(saved_file):
+        print(f'Getting Tracts for {geoid}')
+        print(download_url)
+        urllib.request.urlretrieve(download_url, saved_file)
 
-    # unzip the file to saved_file 
+    with zipfile.ZipFile(saved_file, 'r') as zip_ref:
+        zip_ref.extractall(download_directory)
